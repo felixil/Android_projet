9ed15375caf369ba2164b31c22e5b85801c138bc
felixil <imbfe1233046@etu.cegepjonquiere.ca>
2018-04-03 00:08:48 -0400

    literation 1

--- Code/Mylifeasalazybaconsimulatorthestudentdeadlineattack/app/src/main/java/dicj/info/imbfe1233046/mylifeasalazybaconsimulatorthestudentdeadlineattack/FirstStep.java
+++ Code/Mylifeasalazybaconsimulatorthestudentdeadlineattack/app/src/main/java/dicj/info/imbfe1233046/mylifeasalazybaconsimulatorthestudentdeadlineattack/FirstStep.java
@@ -1,4 +1,5 @@
 package dicj.info.imbfe1233046.mylifeasalazybaconsimulatorthestudentdeadlineattack;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -10,11 +11,14 @@ import android.widget.RelativeLayout;
  * Created by ImbFe1233046 on 2018-03-27.
  */
 public class FirstStep extends Activity implements OnTouchListener {
-
+    MediaPlayer mp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.firststep);
+        mp=MediaPlayer.create(this,R.raw.main_song);
+        mp.setLooping(true);
+        mp.start();
         RelativeLayout layout= (RelativeLayout)findViewById(R.id.ecranStart);
         layout.setOnTouchListener(this);
     }
